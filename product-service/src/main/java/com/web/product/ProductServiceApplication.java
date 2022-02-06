23a1cdaefb24cc3fbc5ba0a0dd4f840c4e1115cf
Rashed <rashedulislam8741@yahoo.com>
2022-02-06 23:19:58 +0600

    Added eureka configuration

--- product-service/src/main/java/com/web/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/web/product/ProductServiceApplication.java
@@ -3,9 +3,12 @@ package com.web.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
